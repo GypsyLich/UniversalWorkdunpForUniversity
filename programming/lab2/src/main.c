4276b0310ea209ec29b20df484270395671a1f0a
Michael <goowillprevail@gmaiil.com>
2019-03-09 23:01:47 +0700

    change vector from void* to int

--- programming/lab2/src/main.c
+++ programming/lab2/src/main.c
@@ -1,7 +1,16 @@
 #include "vector.h"
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char const *argv[]) {
-    /* code */
+int main() {
+    vector *v = vector_init(5);
+    size_t n = (size_t)(vector_get_capacity(v) + 3);
+    for (size_t i = 0; i < n; ++i) {
+        vector_push_back(v, (int)i);
+        printf("%d\n", vector_get_item(v, i));
+    }
+    vector_free(v);
     return 0;
 }
--- programming/lab2/src/vector.c
+++ programming/lab2/src/vector.c
@@ -4,29 +4,43 @@
 #include <stdlib.h>
 #include <string.h>
 
-void vector_init(vector *v) {
-    v->data = NULL;
-    v->size = 0;
-    v->capacity = 0;
+vector *vector_init(size_t initial_capacity) {
+    vector *v = malloc(sizeof(*v));
+    if (v != NULL) {
+        if (initial_capacity > 0) {
+            v->data = malloc(sizeof(int) * initial_capacity);
+            memset(v->data, '\0', sizeof(int) * initial_capacity);
+            v->size = 0;
+            v->capacity = initial_capacity;
+        }
+    }
+    return v;
+}
+
+vector *vector_copy(const vector *v) {
+    vector *vCopy = vector_init(v->capacity);
+    memcpy(vCopy, v, v->size);
+    vCopy->size = v->size;
+    return vCopy;
 }
 
 size_t vector_get_size(vector *v) { return v->size; }
 
 size_t vector_get_capacity(vector *v) { return v->capacity; }
 
-int vector_push_back(vector *v, void *e) {
+int vector_push_back(vector *v, int e) {
     if (v->capacity == 0) {
         v->capacity = 2;
         v->data = malloc(sizeof(void *) * v->capacity);
         if (v->data == NULL) {
             return -1;
         }
-        memset(v->data, '\0', sizeof(void *) * v->capacity);
+        memset(v->data, '\0', sizeof(int) * v->capacity);
     }
 
     if (v->capacity == v->size) {
         v->capacity *= 2;
-        v->data = realloc(v->data, sizeof(void *) * v->capacity);
+        v->data = realloc(v->data, sizeof(int) * v->capacity);
     }
 
     v->data[v->size] = e;
@@ -34,31 +48,36 @@ int vector_push_back(vector *v, void *e) {
     return 0;
 }
 
-void vector_pop_back(vector *v) {}
-
 void vector_shrink_to_fit(vector *v) {
     if (v->capacity != v->size) {
         v->capacity = v->size;
-        v->data = realloc(v->data, sizeof(void *) * v->capacity);
+        v->data = realloc(v->data, sizeof(int) * v->capacity);
     }
 }
 
 int vector_resize(vector *v, size_t new_size) {
     if (v->size < new_size) {
-        for (size_t i = v->size + 1; i = < new_size; ++i) {
+        v->capacity = new_size;
+        memset(&v->data[v->size], 0, sizeof(int) * (v->capacity - 1));
+        v->size = new_size;
+        return 0;
+        /*for (size_t i = v->size + 1; i = new_size; ++i) {
             vector_push_back(v, 0);
-        }
+        }*/
     }
+    return -1;
 }
 
 int vector_reserve(vector *v, size_t new_capacity) {
     if (v->capacity < new_capacity) {
         v->capacity = new_capacity;
-        v->data = realloc(v->data, sizeof(void *) * v->capacity);
+        v->data = realloc(v->data, sizeof(int) * v->capacity);
+        return 0;
     }
+    return -1;
 }
 
-void vector_set_item(vector *v, size_t index, void *e) {
+void vector_set_item(vector *v, size_t index, int e) {
     if (index >= v->size) {
         return;
     }
@@ -66,9 +85,9 @@ void vector_set_item(vector *v, size_t index, void *e) {
     v->data[index] = e;
 }
 
-void *vector_get_item(vector *v, size_t index) {
+int vector_get_item(vector *v, size_t index) {
     if (index >= v->size) {
-        return NULL;
+        return -1;
     }
 
     return v->data[index];
@@ -87,4 +106,7 @@ void vector_delete(vector *v, size_t index) {
     v->size--;
 }
 
-void vector_free(vector *v) { free(v->data); }
+void vector_free(vector *v) {
+    free(v->data);
+    free(v);
+}
